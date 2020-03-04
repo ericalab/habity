72ca6bd89864ce9d86e169abe8729e0be35f9795
横山恵利香 <yokoyamaerika@yokoyamaerikanoMacBook-Pro.local>
2020-03-04 17:12:39 +0900

    Added categories

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,14 +30,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: '495cc78e9f3548b389f42d3bef432c38.vfs.cloud9.us-east-2.amazonaws.com', protocol: 'https' }
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            Rails.application.credentials.gmail[:user_name],
-  password:             Rails.application.credentials.gmail[:password],
-  authentication:       'login',
-  enable_starttls_auto: true }
+  # config.action_mailer.smtp_settings = {
+  # address:              'smtp.gmail.com',
+  # port:                 587,
+  # domain:               'gmail.com',
+  # user_name:            Rails.application.credentials.gmail[:user_name],
+  # password:             Rails.application.credentials.gmail[:password],
+  # authentication:       'login',
+  # enable_starttls_auto: true }
   
   
   config.action_mailer.perform_caching = false
